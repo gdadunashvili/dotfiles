6c56a03e8f1772172f29f381ce1690128b77b6bd
dadu <g.dadunashvili@mailbox.org>
2024-04-25 15:07:28 +0200

    unmapped unneessary keys in lsp

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -73,8 +73,8 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<Esc>'] = cmp.mapping.close(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
     }),
     sources = {
         { name = 'nvim_lsp' }, -- For nvim-lsp
