ba59b28ef0a57173f9f4d901d512b608dc35ec93
dadu <g.dadunashvili@mailbox.org>
2025-03-27 23:28:12 +0100

    Added more detail to ltex-ls config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -172,7 +172,45 @@ return {
 
             ["ltex"] = function ()
             lspconfig.ltex.setup({
-                language = "en-US",
+                filetypes = { "latex", "tex", "bib", "markdown", "gitcommit", "text" },
+                settings = {
+                    ltex = {
+                        enabled = { "latex", "tex", "bib", "markdown", },
+                        textareas = { "comment", "text" },
+                        language = "auto",
+                        diagnosticSeverity = "information",
+                        sentenceCacheSize = 2000,
+-- thiss is an misspellled comment
+                        additionalRules = {
+                            enablePickyRules = true,
+                            enableHunspell = true,
+                            motherTongue = "en-US",
+                        },
+                    },
+                },
+                dictionary = (function()
+                    -- For dictionary, search for files in the runtime to have
+                    -- and include them as externals the format for them is
+                    -- dict/{LANG}.txt
+                    --
+                    -- Also add dict/default.txt to all of them
+                    local files = {}
+                    for _, file in ipairs(vim.api.nvim_get_runtime_file("dict/*", true)) do
+                        local lang = vim.fn.fnamemodify(file, ":t:r")
+                        local fullpath = vim.fs.normalize(file, ":p")
+                        files[lang] = { ":" .. fullpath }
+                    end
+
+                    if files.default then
+                        for lang, _ in pairs(files) do
+                            if lang ~= "default" then
+                                vim.list_extend(files[lang], files.default)
+                            end
+                        end
+                        files.default = nil
+                    end
+                    return files
+                end)(),
             })
             end
 
@@ -182,6 +220,7 @@ return {
             ensure_installed =  { "lua_ls",
                                   "clangd",
                                   "bashls",
+                                  "ltex-ls",
                               },
             automatic_installation = true,
             handlers=handlers,
