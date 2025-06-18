fb4154d8ba11dee38cd43b7ddaa33e124e38df7d
George Dadunashvili <g.dadunashvili@mailbox.org>
2025-06-18 20:29:30 +0200

    nvim: Better gutter for folding

--- config/nvim/lua/plugins/ufo_fold.lua
+++ config/nvim/lua/plugins/ufo_fold.lua
@@ -28,7 +28,23 @@ end
 
 return {
     'kevinhwang91/nvim-ufo', 
-    dependencies = {'kevinhwang91/promise-async'},
+    dependencies = {
+        'kevinhwang91/promise-async',
+        {
+            "luukvbaal/statuscol.nvim",
+            config = function()
+                local builtin = require("statuscol.builtin")
+                require("statuscol").setup({
+                    relculright = true,
+                    segments = {
+                        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+                        { text = { "%s" }, click = "v:lua.ScSa" },
+                        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+                    },
+                })
+            end,
+        },
+    },
     config = function ()
         local ufo = require('ufo')
 
@@ -37,6 +53,7 @@ return {
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
+        vim.o.fillchars = [[eob: ,fold:.,foldopen:,foldsep: ,foldclose:]]
 
         -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
         vim.keymap.set('n', 'zR', ufo.openAllFolds)
