470afeadd2516ff8a14fbc8995b24770f0a2c947
George Dadunashvili <g.dadunashvili@mailbox.org>
2025-07-22 21:08:35 +0200

    nvim: make current line highligt color more pronounced

--- config/nvim/lua/plugins/colorscemes.lua
+++ config/nvim/lua/plugins/colorscemes.lua
@@ -1,13 +1,35 @@
+
+local function darken_color(color, amount)
+    local r = tonumber(color:sub(2, 3), 16)
+    local g = tonumber(color:sub(4, 5), 16)
+    local b = tonumber(color:sub(6, 7), 16)
+
+    r = math.max(0, r - amount)
+    g = math.max(0, g - amount)
+    b = math.max(0, b - amount)
+
+    return string.format("#%02x%02x%02x", r, g, b)
+end
+
+local function update_cursor()
+    local bg_color = vim.api.nvim_get_hl_by_name("Normal", true).background
+    local bg_hex = string.format("#%06x", bg_color)
+    local darker_bg = darken_color(bg_hex, 60)
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = darker_bg })
+end
+
 local function set_comments()
     vim.api.nvim_set_hl(0, "BlameLineNvim", { fg = "#8B0089", ctermfg = 245 })
 end
 
 local function dark_f()
     vim.api.nvim_set_option_value('background', 'dark', {})
+    update_cursor()
 end
 
 local function light_f()
     vim.api.nvim_set_option_value('background', 'light', {})
+    update_cursor()
 end
 
 local function is_dark()
