9296692e3915c7f475528952cff55d841b3a4247
George Dadunashvili <g.dadunashvili@mailbox.org>
2025-08-27 22:42:36 +0200

    keyd: added keyd

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,33 @@
 # Sets:
 
 
+make_backup_dir(){
+    if [ ! -d "$backup_dir" ]; then
+        echo "Creating backup directory $backup_dir to store existing files"
+        mkdir -p $backup_dir
+    fi
+}
+
+install_keymaps(){
+    source_file="config/keyd/default.conf"
+    target="/etc/keyd/default.conf"
+    if [ -h $target ]; then
+        echo "Deleting old symlink"
+        sudo rm $target
+    fi
+
+    make_backup_dir
+    # If it is a file, back it up if not already done so
+    if [ -f $target ] && [ -d $backup_dir ]; then
+        echo "Backing up existing file"
+        cp -i $target $backup_dir/
+    fi
+
+    echo $PWD/$source_file
+    sudo ln -s $PWD/$source_file $target
+    sudo keyd reload
+}
+
 set_editor="config/nvim editorconfig"
 
 # Typicall full workstation, not including any window manager
@@ -31,7 +58,10 @@ backup_dir=~/.dotfiles_old
 
 case "$1" in
     full)
-        install=$set_full
+        install=$set_full ;;
+    keymaps)
+        install_keymaps
+        install="config/sxhkd/sxhkdrc"
         ;;
     editor)
         install=$set_editor;;
@@ -45,17 +75,14 @@ case "$1" in
         echo -e "Available sets:\n"
         echo -e "full:\n$set_full\n"
         echo -e "editor:\n$set_full\n"
+        echo -e "keymaps:\nsets up keyd and sxhkd configs\n"
         echo -e "git_template:\nSetup git templates for ctags\n"
         echo -e "set_fonts:\nSetup a few nerdfonts\n"
         exit 1;;
 esac
 
 
-if [ ! -d "$backup_dir" ]; then
-    echo "Creating backup directory $backup_dir to store existing files"
-    mkdir -p $backup_dir
-fi
-
+make_backup_dir
 
 for file in $install; do
     echo "Processing ~/.$file"
