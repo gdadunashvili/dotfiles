4d9242e020947b872557f530476521883a2c38e0
George Dadunashvili <g.dadunashvili@mailbox.org>
2025-10-23 15:53:41 +0200

    nvim: autoformat all files not only c++

--- config/nvim/lua/config/config.lua
+++ config/nvim/lua/config/config.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
 vim.api.nvim_create_autocmd({"BufWritePre"},
 {
     group = "FormatOnSave",
-    pattern = { "*.cpp", "*.h", "*.hpp", "*.c" },
+    pattern = {"*"},
     callback= function ()
         vim.lsp.buf.format();
     end
