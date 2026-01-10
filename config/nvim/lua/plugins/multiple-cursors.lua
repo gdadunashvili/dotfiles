cba874d0001c0e4e1f8ee95e26aae23698a305ed
dadu <g.dadunashvili@mailbox.org>
2026-01-10 22:20:58 +0100

    nvim: add language-injection plugin & minor changes to multiple-cursors

--- config/nvim/lua/plugins/multiple-cursors.lua
+++ config/nvim/lua/plugins/multiple-cursors.lua
@@ -1,6 +1,17 @@
 return {
     "brenton-leighton/multiple-cursors.nvim",
     version = "*", -- Use the latest tagged version
+    post_hook = function()
+        -- Set MultipleCursorsCursor to be slightly darker than Cursor
+        local cursor = vim.api.nvim_get_hl(0, { name = "Cursor" })
+        cursor.bg = cursor.bg - 3355443 -- -#333333
+        vim.api.nvim_set_hl(0, "MultipleCursorsCursor", cursor)
+
+        -- Set MultipleCursorsVisual to be slightly darker than Visual
+        local visual = vim.api.nvim_get_hl(0, { name = "Visual" })
+        visual.bg = visual.bg - 1118481 -- -#111111
+        vim.api.nvim_set_hl(0, "MultipleCursorsVisual", visual)
+    end,
     opts = {
         custom_key_maps = {
             { "n", "<Leader>|", function() require("multiple-cursors").align() end },
@@ -8,14 +19,13 @@ return {
     }, -- This causes the plugin setup function to be called
     keys = {
         { "<C-S-j>",       "<Cmd>MultipleCursorsAddDown<CR>",        mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
+        { "<C-Down>",      "<Cmd>MultipleCursorsAddDown<CR>",        mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
         { "<C-S-k>",       "<Cmd>MultipleCursorsAddUp<CR>",          mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
         { "<C-Up>",        "<Cmd>MultipleCursorsAddUp<CR>",          mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
-        { "<C-Down>",      "<Cmd>MultipleCursorsAddDown<CR>",        mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
         { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" },      desc = "Add or remove cursor" },
         { "<Leader>vv",    "<Cmd>MultipleCursorsAddVisualArea<CR>",  mode = { "x" },           desc = "Add cursors to the lines of the visual area" },
 
         { "<Leader>va",    "<Cmd>MultipleCursorsAddMatches<CR>",     mode = { "n", "x" },      desc = "Add cursors to cword" },
-        { "<Leader>vA",    "<Cmd>MultipleCursorsAddMatchesV<CR>",    mode = { "n", "x" },      desc = "Add cursors to cword in previous area" },
 
         { "<Leader>vl",    "<Cmd>MultipleCursorsLock<CR>",           mode = { "n", "x" },      desc = "Lock virtual cursors" },
     },
---
+++ config/nvim/lua/plugins/treesitter-language-injection.lua
@@ -0,0 +1,7 @@
+return {
+    "dariuscorvus/tree-sitter-language-injection.nvim",
+    opts = {}, -- calls setup()
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+}
