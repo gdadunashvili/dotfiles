162cf7517262ec2b814cbe5c923d74511c97b1d4
dadu <g.dadunashvili@mailbox.org>
2026-01-11 13:35:16 +0100

    nvim: new plugins git page, smooth scroll, jump to places with flash

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,17 @@
 -- lazy.nvim
-return { "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    picker = { enabled = true, },
-    image = { enabled = true, },
-  }
+return {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+        picker = { enabled = true, },
+        image = { enabled = true, },
+    },
+    gitbrowse = {
+        -- your gitbrowse configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    keys = {
+        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    }
 }
