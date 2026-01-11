1dbc260b03915076670a7b205fb0076df71c7247
dadu <g.dadunashvili@mailbox.org>
2026-01-11 13:50:46 +0100

    nvim: activate snacks terminal

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -6,12 +6,15 @@ return {
         picker = { enabled = true, },
         image = { enabled = true, },
     },
-    gitbrowse = {
-        -- your gitbrowse configuration comes here
+    gitbrowse = {},
+    terminal = {
+        -- your terminal configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
     },
     keys = {
-        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse",      mode = { "n", "v" } },
+        { "<c-/>",      function() Snacks.terminal() end,  desc = "Toggle Terminal" },
+        { "<c-_>",      function() Snacks.terminal() end,  desc = "which_key_ignore" },
     }
 }
