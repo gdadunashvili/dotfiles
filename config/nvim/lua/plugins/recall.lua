70c0fe7def6dfef2aa08bcd221859a91a1c1c394
dadu <g.dadunashvili@mailbox.org>
2026-03-23 11:27:44 +0100

    nvim: tweaks to recall

--- config/nvim/lua/plugins/recall.lua
+++ config/nvim/lua/plugins/recall.lua
@@ -1,8 +1,25 @@
 return {
     "fnune/recall.nvim",
     config = function()
-        require("recall").setup {}
-        vim.keymap.set("n", "<leader>rt", ":Telescope recall<CR>")
-        vim.keymap.set("n", "<leader>rb", ":RecallToggle<CR>")
+        local recall = require("recall")
+        recall.setup({
+            sign = "",
+            sign_highlight = "@comment.note",
+
+            telescope = {
+                autoload = true,
+                mappings = {
+                    unmark_selected_entry = {
+                        normal = "dd",
+                        insert = "",
+                    },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "mm", recall.toggle, { noremap = true, silent = true })
+        vim.keymap.set("n", "mj", recall.goto_next, { noremap = true, silent = true })
+        vim.keymap.set("n", "mk", recall.goto_prev, { noremap = true, silent = true })
+        vim.keymap.set("n", "ml", ":Telescope recall<CR>", { noremap = true, silent = true })
     end,
 }
