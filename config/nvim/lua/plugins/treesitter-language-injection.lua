fbdc1093e62d97cab1c678a50045c3bc7739daf6
dadu <g.dadunashvili@mailbox.org>
2026-05-19 09:35:09 +0200

    nvim: upgrade to new treesitter

--- config/nvim/lua/plugins/treesitter-language-injection.lua
+++ config/nvim/lua/plugins/treesitter-language-injection.lua
@@ -1,7 +1,7 @@
 return {
-    "dariuscorvus/tree-sitter-language-injection.nvim",
-    opts = {}, -- calls setup()
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-    },
+    -- "dariuscorvus/tree-sitter-language-injection.nvim",
+    -- opts = {}, -- calls setup()
+    -- dependencies = {
+    -- "nvim-treesitter/nvim-treesitter",
+    -- },
 }
