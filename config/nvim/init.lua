4e2f4cbae0768b48c7a272d221af30077310aeea
George Dadunashvili <q666464@clid2140438.muc>
2026-06-02 17:13:23 +0200

    nvim: pcall local lua files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,13 @@
 require("config.lazy")
 require("lazy").setup("plugins")
+require("config.init")
 
-require("config")
-pcall( require "local/init")
+local ok, _ = pcall(function() require("local.init") end)
+if ok then
+    local ok_init, _ = pcall(require "local.init")
+    if ok_init then
+        require("local.init")
+    end
+else
+    vim.notify("no local config")
+end
