5a96b6519f36d97b20a9321d93609d08759fc9c9
George Dadunashvili <george.dadunashvili@bmw.de>
2026-08-21 11:55:27 +0200

    nvim: better flash search

--- config/nvim/lua/plugins/camelcasemotion.lua
+++ config/nvim/lua/plugins/camelcasemotion.lua
@@ -1,12 +1,11 @@
 return {
-  'bkad/CamelCaseMotion',
-  config = function()
-      -- vim.cmd("let g:camelcasemotion_key = '<leader>'")
-      -- vim.g.camelcasemotion_key ='<leader>'
-      vim.keymap.set('o', 'iw', '<Plug>CamelCaseMotion_iw', { silent = true })
-      vim.keymap.set({'n', 'v'}, 'w', '<Plug>CamelCaseMotion_w', { silent = true })
-      vim.keymap.set({'n', 'v'}, 'b', '<Plug>CamelCaseMotion_b', { silent = true })
-      vim.keymap.set({'n', 'v'}, 'e', '<Plug>CamelCaseMotion_e', { silent = true })
-      vim.keymap.set({'n', 'v'}, 'ge', '<Plug>CamelCaseMotion_ge', { silent = true })
-  end,
+    'bkad/CamelCaseMotion',
+    config = function()
+        -- vim.cmd("let g:camelcasemotion_key = '<leader>'")
+        -- vim.g.camelcasemotion_key ='<leader>'
+        vim.keymap.set({ 'n', 'v' }, 'w', '<Plug>CamelCaseMotion_w', { silent = true })
+        vim.keymap.set({ 'n', 'v' }, 'b', '<Plug>CamelCaseMotion_b', { silent = true })
+        vim.keymap.set({'n', 'v'}, 'e', '<Plug>CamelCaseMotion_e', { silent = true })
+        vim.keymap.set({'n', 'v'}, 'ge', '<Plug>CamelCaseMotion_ge', { silent = true })
+    end,
 }
--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -1,10 +1,77 @@
+local browser_jump = function()
+    local Flash = require("flash")
+
+    ---@param opts Flash.Format
+    local function format(opts)
+        -- always show first and second label
+        return {
+            { opts.match.label1, "FlashMatch" },
+            { opts.match.label2, "FlashLabel" },
+        }
+    end
+
+    Flash.jump({
+        search = { mode = "search" },
+        label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+        pattern = [[\<]],
+        action = function(match, state)
+            state:hide()
+            Flash.jump({
+                search = { max_length = 0 },
+                highlight = { matches = false },
+                label = { format = format },
+                matcher = function(win)
+                    -- limit matches to the current label
+                    return vim.tbl_filter(function(m)
+                        return m.label == match.label and m.win == win
+                    end, state.results)
+                end,
+                labeler = function(matches)
+                    for _, m in ipairs(matches) do
+                        m.label = m.label2 -- use the second label
+                    end
+                end,
+            })
+        end,
+        labeler = function(matches, state)
+            local labels = state:labels()
+            for m, match in ipairs(matches) do
+                match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                match.label2 = labels[(m - 1) % #labels + 1]
+                match.label = match.label1
+            end
+        end,
+    })
+end
+
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+        modes = {
+            search = { enabled = false },
+            char = {
+                enabled = false,
+                keys = {},
+            },
+        },
+    },
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+
+        { "f", mode = { "n", "x", "o" }, browser_jump,                                 desc = "Flash but like in the browser" },
+        { "F", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+        { "t", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+        {
+            "#",
+            mode = { "n", "x", "o" },
+            function()
+                require("flash").jump({
+                    pattern = vim.fn.expand("<cword>"),
+                })
+            end,
+            desc = "Flash Treesitter Search"
+        },
     },
 }
